be306c30c1bf3b701a5c75aa4b8f157743c0fd78
WJFeiOne <ww2233597457@163.com>
2019-10-08 00:15:02 +0800

    03添加路由守卫

--- src/main.js
+++ src/main.js
@@ -21,6 +21,27 @@ Vue.prototype.$axios = axios;
 // 设置 基准路径
 axios.defaults.baseURL="http://localhost:3000";
 
+// 添加 路由守卫
+router.beforeEach((to, from, next) => {
+    // 判断 非登录页，都必须判断是否有 token
+    if(to.path !== "/login"){
+        // 获取 本地用户数据
+        const token = JSON.parse(localStorage.getItem("user") || `{}`).token;
+        // 判断是否有 token 值
+        if(token){
+            // 有 token 正常跳转
+            next()
+        }else{
+            // 没有 token 返回登录页
+            next("/login")
+        }
+    }else{
+        // 正常访问 登录页面
+        next();
+    }
+})
+
+
 // 创建 Vue 根实例 
 new Vue({
     router,               // 挂载 路由到根实例    
